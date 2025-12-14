5bfa7f595cf1e03b17ad60dc79ca67d9d55da7fb
SummerEcho <164468785+EchosOfSummer@users.noreply.github.com>
2025-12-13 19:07:35 -0600

    done

--- public/scripts/event.js
+++ public/scripts/event.js
@@ -127,22 +127,3 @@
     }
 })()
 
-
-    // const { pathname } = window.location
-    // const parts = pathname.split('/')
-    // const [, searchType, id ] = parts
-
-    // //if (searchType !== 'event' || !id) return
-
-    // const res = await fetch(`/api/v1/events/${id}`)
-    // const { name, location, date, time } = await res.json()
-
-    // const nameEl = document.querySelector('#eventName')
-    // const locationEl = document.querySelector('#eventLocation')
-    // const dateEl = document.querySelector('#eventDate')
-    // const timeEl = document.querySelector('#eventTime')
-
-    // if (nameEl) nameEl.textContent = name
-    // if (locationEl) locationEl.textContent = `Location: ${location || ''}` 
-    // if (dateEl) dateEl.textContent = `Date: ${date || ''}`
-    // if (timeEl) timeEl.textContent = `Time: ${time || ''}`
